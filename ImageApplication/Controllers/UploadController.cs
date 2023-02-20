e7ddf6f1d92f0e8d1dbac7391d596e6a66c9ce91
Mohammed Fananah <Hp@HP>
2023-02-20 10:38:29 +0200

    Add project files.

---
+++ ImageApplication/Controllers/UploadController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ImageApplication.Data;
+using ImageApplication.Models.ViewModel;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using ImageApplication.Models;
+
+namespace ImageApplication.Controllers
+{
+    public class UploadController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IHostingEnvironment environment;
+
+        public UploadController(ApplicationDbContext context,IHostingEnvironment environment)
+        {
+            this.context = context;
+            this.environment = environment;
+        }
+
+        [HttpGet]
+        public IActionResult UploadImage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UploadImage(ImageCreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var path = environment.WebRootPath;
+                var filePath = "Content/Image/" + model.ImagePath.FileName;
+                var fullPath = Path.Combine(path, filePath);
+                UploadFile(model.ImagePath, fullPath);
+                var data = new Image()
+                {
+                    Name = model.Name,
+                    ImagePath=filePath
+                };
+                context.Add(data);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            else
+            {
+                return View(model);
+            }
+        }
+        
+        public void UploadFile(IFormFile file,string path)
+        {
+            FileStream stream = new FileStream(path, FileMode.Create);
+            file.CopyTo(stream);
+        }
+       
+
+        public IActionResult Index()
+        {
+            var data = context.Images.ToList();
+            return View(data);
+        }
+
+    }
+}
---
+++ ImageApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ImageApplication.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ImageApplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Image> Images { get; set; }
+    }
+}
